f879c9216b7b62764dfd9c396c87af633430989f
MrNtex <ntexyt@gmail.com>
2026-03-05 21:05:30 +0100

    Add project files.

---
+++ Arena.cpp
@@ -0,0 +1,33 @@
+#include <memory>
+#include <new>
+#include <type_traits>
+
+template <std::size_t BlockSize>
+struct Arena
+{
+	alignas(std::max_align_t) std::byte buffer[BlockSize];
+	void* current{ buffer };
+	std::size_t space_left{ BlockSize };
+
+	template<typename T, typename... Args>
+	T* allocate(Args&&... args)
+	{
+		static_assert(std::is_trivially_destructible_v<T>, "T must be trivially destructible")
+
+		if (std::align(alignof(T), sizeof(T), current, space_left))
+		{
+			T* obj = new (current) T(std::forward<Args>(args)...);
+			current = static_cast<std::byte*>(current) + sizeof(T);
+			space_left -= sizeof(T);
+			return obj;
+		}
+		throw std::bad_alloc();
+		//return nullptr;
+	}
+
+	void reset() noexcept
+	{
+		current = buffer;
+		space_left = BlockSize;
+	}
+};
---
+++ Arena_and_Pool_Allocators.cpp
@@ -0,0 +1,12 @@
+﻿// Arena_and_Pool_Allocators.cpp : Defines the entry point for the application.
+//
+
+#include "Arena_and_Pool_Allocators.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
