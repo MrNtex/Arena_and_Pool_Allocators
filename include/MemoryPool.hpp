37a5bc9452a88cdaa7851e01b60bca69a0a27049
MrNtex <ntexyt@gmail.com>
2026-03-06 17:33:29 +0100

    feat: Added SizeClassPool and untyped memory pool

--- include/MemoryPool.hpp
+++ include/MemoryPool.hpp
@@ -76,3 +76,76 @@ public:
 		head = node;
 	}
 };
+
+template<std::size_t BlockSize, std::size_t PoolSize>
+class SizeClassPool
+{
+private:
+	static constexpr std::size_t Alignment = std::min(alignof(std::max_align_t), BlockSize);
+	union Block
+	{
+		Block* next;
+		alignas(Alignment) std::byte data[BlockSize];
+	};
+
+	static_assert(BlockSize >= sizeof(Block*), "BlockSize must fit a 64-bit pointer.");
+
+	Block buffer[PoolSize];
+	Block* head{ nullptr };
+public:
+	SizeClassPool()
+	{
+		for (std::size_t i = 0; i < PoolSize; ++i)
+		{
+			auto* node = new (&buffer[i]) Block{ head };
+			head = node;
+		}
+	}
+
+	void* allocate()
+	{
+		if (!head) throw std::bad_alloc();
+
+		Block* free_block = head;
+		head = head->next;
+
+		return free_block->data;
+	}
+
+	void deallocate(void* ptr)
+	{
+		Block* node = new (ptr) Block{ head };
+		head = node;
+	}
+};
+
+template<std::size_t PoolSize>
+class MemoryPool
+{
+private:
+	SizeClassPool<8, PoolSize> pool_8;
+	SizeClassPool<16, PoolSize> pool_16;
+	SizeClassPool<64, PoolSize> pool_64;
+	SizeClassPool<256, PoolSize> pool_256;
+
+public:
+	void* allocate(std::size_t size)
+	{
+		if (size <= 8) return pool_8.allocate();
+		if (size <= 16) return pool_16.allocate();
+		if (size <= 64) return pool_64.allocate();
+		if (size <= 256) return pool_256.allocate();
+		//else throw std::bad_alloc();
+
+		return ::operator new(size);
+	}
+
+	void deallocate(void* ptr, std::size_t size)
+	{
+		if (size <= 8) pool_8.deallocate(ptr);
+		else if (size <= 16) pool_16.deallocate(ptr);
+		else if (size <= 64) pool_64.deallocate(ptr);
+		else if (size <= 256) pool_256.deallocate(ptr);
+		else ::operator delete(ptr);
+	}
+};
